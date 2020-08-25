2f23e7fdf544a3d422b824ff5afe20957457a071
nardo88 <nardo1988@mail.ru>
2020-08-25 13:29:19 +0300

    'new'

--- Theory/Css/grid/script.js
+++ Theory/Css/grid/script.js
@@ -3,8 +3,10 @@ const example = document.querySelectorAll('.example')
 const example2btn = document.querySelectorAll('.img_btn')
 const example2 = document.querySelector('.example2')
 const gridItem4 = document.querySelectorAll('.grid-item4')
-
-
+const gridItem5 = document.querySelectorAll('.example5 .grid-item')
+const example6 = document.querySelector('.example6')
+const gridItem6_3 = document.querySelector('.grid-item6_3')
+const example7 = document.querySelector('.example7')
 
 example.forEach((item, i) => {
     item.addEventListener('click', () => {
@@ -35,6 +37,9 @@ example2btn.forEach((item, i) => {
         if (i == 4){
             example2.style.gridTemplateColumns = '1fr minmax(150px, 1fr) 1fr'
         }
+        if (i == 5){
+            example2.style.gridTemplateColumns = 'repeat(3, 1fr)'
+        }
         if (i == 6){
             gridItem4.forEach((item) => {
                 item.style.margin = '20px'
@@ -48,6 +53,62 @@ example2btn.forEach((item, i) => {
         }
         if (i == 9){
             gridItem4[0].classList.toggle('grid-item4_3')
+        }
+        if (i == 10){
+            gridItem5[0].style.order = 2
+            gridItem5[1].style.order = 3
+            gridItem5[2].style.order = 4
+            gridItem5[3].style.order = 5
+            gridItem5[4].style.order = 1
+            gridItem5[5].style.order = 6
+        }
+        if (i == 11){
+            gridItem5[0].style.order = 6
+            gridItem5[1].style.order = 5
+            gridItem5[2].style.order = 4
+            gridItem5[3].style.order = 3
+            gridItem5[4].style.order = 2
+            gridItem5[5].style.order = 1
+        }
+        if (i == 12){
+            gridItem5[0].style.order = 1
+            gridItem5[1].style.order = 2
+            gridItem5[2].style.order = 3
+            gridItem5[3].style.order = 4
+            gridItem5[4].style.order = 5
+            gridItem5[5].style.order = 6
+        }
+        if (i == 13){
+            example6.style.justifyItems = 'stretch'
+            example6.style.alignItems = 'stretch'
+        }
+        if (i == 14){
+            example6.style.justifyItems = 'start'
+            example6.style.alignItems = 'start'
+        }
+        if (i == 15){
+            example6.style.justifyItems = 'end'
+            example6.style.alignItems = 'end'
+        }
+        if (i == 16){
+            example6.style.justifyItems = 'center'
+            example6.style.alignItems = 'center'
+        }
+        if (i == 17){
+            gridItem6_3.style.justifySelf = 'start' 
+            gridItem6_3.style.alignSelf = 'start'
+        }
+        if (i == 18){
+            example7.style.rowGap = '20px' 
+           
+        }
+        if (i == 19){
+            example7.style.columnGap = '20px' 
+           
+        }
+        if (i == 20){
+            example7.style.gap = '0'
+           
         }
     })
 })
