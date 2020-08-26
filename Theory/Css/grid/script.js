b39b2e27ff11d188019708b553468222335c73a1
nardo88 <nardo1988@mail.ru>
2020-08-26 19:40:01 +0300

    'new'

--- Theory/Css/grid/script.js
+++ Theory/Css/grid/script.js
@@ -4,14 +4,12 @@ const example2btn = document.querySelectorAll('.img_btn')
 const example2 = document.querySelector('.example2')
 const gridItem4 = document.querySelectorAll('.grid-item4')
 
-
-
 example.forEach((item, i) => {
     item.addEventListener('click', () => {
-        if (i == 0){
+        if (i == 0) {
             example1.style.display = 'grid'
         }
-        if (i == 1){
+        if (i == 1) {
             example1.style.display = 'inline-grid'
         }
         console.log(i);
@@ -20,33 +18,33 @@ example.forEach((item, i) => {
 
 example2btn.forEach((item, i) => {
     item.addEventListener('click', () => {
-        if (i == 0){
+        if (i == 0) {
             example2.style.gridTemplateColumns = '200px'
         }
-        if (i == 1){
+        if (i == 1) {
             example2.style.gridTemplateColumns = '200px 150px 250px'
         }
-        if (i == 2){
+        if (i == 2) {
             example2.style.gridTemplateColumns = '1fr 1fr 1fr'
         }
-        if (i == 3){
+        if (i == 3) {
             example2.style.gridTemplateColumns = '1fr 2fr 1fr'
         }
-        if (i == 4){
+        if (i == 4) {
             example2.style.gridTemplateColumns = '1fr minmax(150px, 1fr) 1fr'
         }
-        if (i == 6){
+        if (i == 6) {
             gridItem4.forEach((item) => {
                 item.style.margin = '20px'
             })
         }
-        if (i == 7){
+        if (i == 7) {
             gridItem4[0].classList.toggle('grid-item4_1')
         }
-        if (i == 8){
+        if (i == 8) {
             gridItem4[0].classList.toggle('grid-item4_2')
         }
-        if (i == 9){
+        if (i == 9) {
             gridItem4[0].classList.toggle('grid-item4_3')
         }
     })
