eeae9ef84e88046fe0dd3dd16aa46b4086e9ef06
nardo88 <nardo1988@mail.ru>
2020-08-27 19:40:49 +0300

    'new'

--- index/firebase/script.js
+++ index/firebase/script.js
@@ -11,3 +11,68 @@ navLinks.forEach(item => {
     })
 })
 
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBj3ZYH8OnzYAGByvcBt4LCxBcdMgfP--g",
+    authDomain: "testfirebase2030.firebaseapp.com",
+    databaseURL: "https://testfirebase2030.firebaseio.com",
+    projectId: "testfirebase2030",
+    storageBucket: "testfirebase2030.appspot.com",
+    messagingSenderId: "667285184129",
+    appId: "1:667285184129:web:41b93bca0225e57525f780"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+let bd = [];
+
+var ref = firebase.database().ref('users/1');
+
+const container = document.querySelector('.wrapper')
+const name_item = document.querySelector('.name_item')
+const age_item = document.querySelector('.age_item')
+const job_item = document.querySelector('.job_item')
+const add_data = document.querySelector('.add_data')
+
+add_data.addEventListener('click', () => {
+    let age = Number(age_item.value)
+
+    bd.push({
+        age: age,
+        job: job_item.value,
+        name: name_item.value,
+
+    })
+   
+    console.log(bd);
+    firebase.database().ref('users/1').set(bd)
+   
+})
+
+ref.on("value", function(snapshot) {
+    bd = []
+    container.innerHTML = ""
+    bd = bd.concat(snapshot.val())
+
+    bd.forEach(item => {
+        let containerItem = `
+        <div class="container__item">
+        <h3 class="item__title">${item.name}</h3>
+        <table>
+            <tr>
+                <td>Возраст</td> <td>${item.age}</td>
+            <tr>
+            <tr>
+                <td>профессия</td> <td>${item.job}</td>
+            <tr>
+        </table>
+        </div>
+        `;
+    
+        container.innerHTML += containerItem
+    })
+
+})
