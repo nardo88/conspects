936f07924158d8f7ac6c8ec8199c211fa2ded337
nardo88 <nardo1988@mail.ru>
2020-09-06 21:26:13 +0300

    'add'

--- Theory/JavaScript/zamikanie/1.js
+++ Theory/JavaScript/zamikanie/1.js
@@ -1,71 +1,16 @@
-// function logPerson() {
-// 	console.log(`Person: ${this.name}, ${this.age}, ${this.job},`)
-// }
 
-// const person1 = {name: 'Михаил', age: 22, job: 'Frontend'}
-// const person2 = {name: 'Елена', age: 19, job: 'SMM'}
+function counter() {
+	let n = 0
 
-
-// function bind(context, fn) {
-// 	return function (...args) {
-// 		fn.apply(context, args)
-// 	}
-// }
-
-// bind(person1, logPerson)()
-// bind(person2, logPerson)()
-
-
-
-function calc(num) {
-	return function (n) {
-		console.log(num + n);
-	}
+	return {
+		count: () => n++,
+		reset: () => n = 0
+		}
 }
 
-const one = calc(10);
-one(20);
-one(55)
+let c = counter()
+let d = counter()
 
 
-function createAdr(domain) {
-	return function (url) {
-		console.log(`https://${url}.${domain}`);
-	}
-}
 
-const getAdr = createAdr('com');
-getAdr('google');
-
-
-function sendInfo() {
-	console.log(`Person: ${this.name}, ${this.age}`);
-}
-
-const person = {name: 'Max', age: 23}
-
-
-function bind(context, fn) {
-	return function (...args) {
-		fn.apply(context, args);
-	}
-}
-
-bind(person, sendInfo)()
-
-// function logPerson() {
-// 	console.log(`Person: ${this.name}, ${this.age}, ${this.job},`)
-// }
-
-// const person1 = {name: 'Михаил', age: 22, job: 'Frontend'}
-// const person2 = {name: 'Елена', age: 19, job: 'SMM'}
-
-
-// function bind(context, fn) {
-// 	return function (...args) {
-// 		fn.apply(context, args)
-// 	}
-// }
-
-// bind(person1, logPerson)()
-// bind(person2, logPerson)()
+	
