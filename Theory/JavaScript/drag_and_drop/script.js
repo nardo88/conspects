b425bd5f4de5279dd176244ecdefbf00f99e10f3
nardo88 <nardo1988@mail.ru>
2020-09-17 11:36:07 +0300

    new

--- Theory/JavaScript/drag_and_drop/script.js
+++ Theory/JavaScript/drag_and_drop/script.js
@@ -47,3 +47,57 @@ child.ondragstart = function() {
 };
 
 
+// Список задач с DnD
+
+// Получаем сылки к элементам
+const tasksListElement = document.querySelector(`.tasks__list`);
+const taskElements = tasksListElement.querySelectorAll(`.tasks__item`);
+// каждому элементу списка добавляем атрибут draggable со значением true
+for (const task of taskElements) {
+    task.draggable = true;
+  }
+//  слушаем событие dragstart при котором каждому таргету добавляем класс
+// selected который меняет цвет фона и прозрачность
+tasksListElement.addEventListener(`dragstart`, (evt) => {
+    evt.target.classList.add(`selected`);
+});
+// после того как буксировка закончена убираем этот класс
+tasksListElement.addEventListener(`dragend`, (evt) => {
+    evt.target.classList.remove(`selected`);
+});
+// dragover срабатывает каждые несколько сотен миллисекунд, пока
+// перетаскиваемый элемент находится над зоной, в которую может быть сброшен.
+tasksListElement.addEventListener(`dragover`, (evt) => {
+    //По умолчанию большинство областей на странице недоступны для сброса.
+    // Чтобы создать область, в которую элементы могут быть сброшены, нужно
+    // внутри обработчика события dragover отменить действие по умолчанию,
+    // переопределив поведение. Сделаем такой областью весь список задач.
+    evt.preventDefault();
+    // Найдём выбранный элемент (с классом selected) и тот элемент, на 
+    // котором сработало событие dragover.
+    const activeElement = tasksListElement.querySelector(`.selected`);
+    const currentElement = evt.target;
+    // Проверим, что событие dragover сработало не на выбранном элементе,
+    // потому что иначе перемещать элемент нет смысла — он уже на нужном месте.
+    // Также проверим, что dragover сработало именно на одном из элементов
+    // списка. Это важно, потому что курсор может оказаться и на пустом 
+    // пространстве между элементами, а оно нас не интересует.
+    const isMoveable = activeElement !== currentElement &&
+        currentElement.classList.contains(`tasks__item`);
+    // Если условие не выполняется, прервём выполнение функции, отменив
+    // все дальнейшие действия.    
+    if (!isMoveable) {
+        return;
+    }
+    // Теперь найдём элемент, перед которым нужно осуществить вставку. 
+    // Сделаем это, сравнив положение выбранного элемента и текущего, 
+    // на который наведён курсор.
+    const nextElement = (currentElement === activeElement.nextElementSibling) ?
+            currentElement.nextElementSibling :
+            currentElement;
+    // Осталось только вставить элемент на новое место. Для этого 
+    // воспользуемся методом insertBefore. Он вызывается на родительском
+    // элементе, первым параметром принимает вставляемый элемент, а
+    // вторым — элемент, перед которым нужно вставить.    
+    tasksListElement.insertBefore(activeElement, nextElement);
+});
